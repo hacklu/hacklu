1dfe76e3b96b7eb4d3cd92e5aaba5a456dfcbd77
hacklu <hacklu.newborn@gmail.com>
2013-06-24 13:23:58 +0800

    [edit] hacklu_cscope.h
    	fix bug

--- hacklu_cscope.sh
+++ hacklu_cscope.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 a=`pwd`
-find $a -name "*.h" -o -name "*.c"  -name "*.S" -o -name "*.s" -o -name "*.cpp" > cscope.files
+find $a -name "*.h" -o -name "*.c" -o -name "*.S" -o -name "*.s" -o -name "*.cpp" > cscope.files
 cscope -bkq -i cscope.files
 ctags -R
